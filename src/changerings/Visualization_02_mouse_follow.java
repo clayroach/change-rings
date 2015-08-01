ead902e38679204d26dca734f12ec1ec152ee3d3
clayroach <clay@j9tech.com>
2015-07-31 22:49:36 -0400

    Added 2nd visualization.

--- src/changerings/Visualization_02_mouse_follow.java
+++ src/changerings/Visualization_02_mouse_follow.java
@@ -31,6 +31,11 @@ public class Visualization_02_mouse_follow extends PApplet {
 	
 	//Start music when person comes into view, otherwise on mouse click
 	boolean START_MUSIC_ON_PERSON = true;
+	
+	//Visualization version
+	// 1 = original
+	// 2 = mouse_follow_print
+	int VERSION = 2;
 
 	Minim minim;
 	AudioPlayer song;
@@ -127,7 +132,6 @@ public class Visualization_02_mouse_follow extends PApplet {
 	// /////////////////// // END DRAW LOOP
 
 	public void draw() {
-		background(0, 0, 0);
 		
 		// MUSIC DOENS'T START UNTIL MOUSE PRESS
 		if (mousePressed) {
@@ -137,7 +141,13 @@ public class Visualization_02_mouse_follow extends PApplet {
 		pushMatrix(); // save off 3d frame of reference before painting 2d
 		hint(DISABLE_DEPTH_TEST); // switch off Z (depth) axis
 
-		drawMusic();
+		switch(VERSION) {
+		case 1: 
+			drawMusic();
+			break;
+		case 2: 
+			drawMusic2();
+		}
 
 		hint(ENABLE_DEPTH_TEST); // restore Z (depth) axis
 		popMatrix(); // restore 3d frame of reference
@@ -316,6 +326,7 @@ public class Visualization_02_mouse_follow extends PApplet {
 
 	void drawMusic() {
 		// .......................................................................................
+		background(0, 0, 0);
 
 		noStroke(); // FADE BACKGROUND
 		fill(0, 20); // 10);
@@ -409,6 +420,80 @@ public class Visualization_02_mouse_follow extends PApplet {
 																		// band_m5*250);
 			rectMode(CORNER);
 		}
+	}
+	
+	void drawMusic2() {
+		c1 = color(random(255), random(129), random(247));
+		//.......................................................................................
+
+		  float[] bands = new float[32];
+
+		/* if (v%RASTRO == 0) {
+		    background(0);
+		  }  */
+		  v++; 
+		  fft.forward(song.mix); 
+		  //fft.forward(in.mix);  
+		  for (int i = 0; i < fft.specSize()/2; i++)
+		  {
+		    bands[i/((fft.specSize()/2)/(bands.length-1))] += fft.getBand(i);
+		  }
+		  //float total = 0.0;            WHY IS THIS HERE??????? (IN THE ORIGINAL) DOING NOTHING??
+
+		 // translate(width/2, height/20);  //height/1.1);   // MORE INTERESTING WHEN IMAGE IS OFF CENTER
+		  float band_m0 = (bands[0]/fft.specSize()/2);
+		  float band_m1 = (bands[1]/fft.specSize()/2);
+		  float band_m2 = (bands[2]/fft.specSize()/2) * 10;
+		  float band_m3 = (bands[3]/fft.specSize()/2) * 10;
+		  float band_m4 = (bands[4]/fft.specSize()/2) * 10;
+		  float band_m5 = (bands[5]/fft.specSize()/2) * 10;
+
+		  if (bands[0] > 1) {
+		    strokeWeight(2);
+		    stroke(255, 255, 0);
+		    //noFill();
+		    fill(255*band_m0, 180, 255); //130);   //  BLUE    //255, 200);  // AQUA //, 100);
+		    ellipse(pmouseX, pmouseY, bands[0]*4, bands[0]*4);    //0, 0, bands[0]*10, bands[0]*10);
+		  } 
+		  if (bands[1] > 20) {
+		    strokeWeight(15);
+		    stroke(0);   //(255, 197, 80);   //, 125);
+		    noFill();
+		    rotate(PI/random(5));
+		    triangle(-10*bands[1], -10*bands[1], mouseX, 10*bands[1], 10*bands[1], -10*bands[1]);
+		   // triangle(random(-width/2, width/2), random(-height/2, height/2), band_m1*28, band_m1*28,10*bands[1], -10*bands[1]);
+		  }
+		  if (bands[2] > 5) {
+		    strokeWeight(10);
+		    stroke(255*band_m2, 100, 155);   //, 75);
+		    noFill();
+		    //fill(255*band_m2, 200, 155, 75);
+		    rotate(PI/random(3));
+		    rect(mouseX, mouseY, 15*bands[2], 15*bands[2]);
+		  } 
+		/*  if (bands[3] > 5) {
+		    strokeWeight(band_m3*20);   //50);   //*100);    SPIRAL BANDS
+		    stroke (0);  //(255*band_m3, 255, 255);   //255, 255, 255);  //*band_m3, 255, 255);  //255, 128);   //, 100);
+		    //fill(255, 255, 255, 150);   // 255*band_m3, 255, 128, 200);    //70);
+		    //ellipse(0, 0, 15*bands[2], 15*bands[2]);
+		    //spiral(500*band_m3, bands[3], (int)(band_m3*2*width));
+		    spiral(pmouseX, pmouseY, (int)(band_m3*2*width));  //50*band_m3, bands[3], (int)(band_m3*2*width));
+		    strokeWeight(0);
+		  } */
+		  if (bands[4] > 1) {
+		    strokeWeight(3);
+		    stroke(255, 0, 0);
+		    fill(c1);  //255, 129, 247);   //, 180);   // ROSE-COLORED SQUARES // (255*band_m4, 255, 255, 200);
+		    rect(random(width/2), random(height/2), band_m4*280, band_m4*280);  //random(-width/2, width/2), random(-height/2, height/2), band_m4*280, band_m4*280);
+		  }
+		  if (bands[5] > 0.5) {
+		    rectMode(CENTER);
+		    strokeWeight(2);
+		    stroke(0);
+		    fill(254, 255, 9);   // YELLOW SQUARES  //255*band_m5); 
+		    rect(width/2, height/2, band_m5*250, band_m5*250);  //random(-width/2, width/2), random(-height/2, height/2), band_m5*250, band_m5*250);
+		   rectMode(CORNER);
+		  } 		
 	}
 
 	// draw the skeleton with the selected joints
